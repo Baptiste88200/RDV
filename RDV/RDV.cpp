7d2220ed6d9c9af7d550c5b66d5e6efb86d79462
ThibaultMilotte <31914114+ThibaultMilotte@users.noreply.github.com>
2020-03-11 14:47:10 +0100

    affichage des triangles d'un fichier obj

--- RDV/RDV.cpp
+++ RDV/RDV.cpp
@@ -13,6 +13,8 @@
 
 using namespace std;
 
+#define DIST_MAX  10000000;
+
 struct Triangle {
     Vec3f p1;
     Vec3f p2;
@@ -20,7 +22,7 @@ struct Triangle {
 
     Triangle(const Vec3f& pt1, const Vec3f& pt2, const Vec3f& pt3) : p1(pt1), p2(pt2), p3(pt3) {}
 
-    bool rayIntersect(Vec3f orig,Vec3f dir) const{
+    bool rayIntersect(Vec3f orig,Vec3f dir, Vec3f& intersection) const{
         const float p = 0.01;
         Vec3f edge1, edge2, h, s, q;
         float a, f, u, v;
@@ -46,6 +48,7 @@ struct Triangle {
         float t = f * (edge2*q);
 
         if (t > p){
+            intersection = orig + (dir * t);
             return true;
         }
         else return false;
@@ -53,12 +56,6 @@ struct Triangle {
 
 };
 
-Vec3f cast_ray(const Vec3f& orig, const Vec3f& dir, const Triangle& tr) {
-    if (!tr.rayIntersect(orig,dir)){
-        return Vec3f(0.2, 0.7, 0.8); // background color
-    }
-    return Vec3f(0.4, 0.4, 0.3);
-}
 
 std::list<Triangle> getTriangle(char* nomFichier) {
     ifstream fichier(nomFichier);
@@ -112,23 +109,53 @@ std::list<Triangle> getTriangle(char* nomFichier) {
     return triangles;
 }
 
-void render() {
+void render(std::list<Triangle> triangles) {
 
-    const int width = 1024;
-    const int height = 768;
+    const int width = 600;
+    const int height = 700;
     const float fov = M_PI / 3.;
+
+
     std::vector<Vec3f> framebuffer(width * height);
 
-    Triangle tr = Triangle(Vec3f(-100,50,-100), Vec3f(0,0,-100), Vec3f(100, 0, -100));
+    Triangle tr = Triangle(Vec3f(-100, 50, -100), Vec3f(0, 0, -100), Vec3f(100, 0, -100));
 
+#pragma omp parallel for
     for (size_t j = 0; j < height; j++) {
         for (size_t i = 0; i < width; i++) {
+
+
             float dir_x = (i + 0.5) - width / 2.;
-            float dir_y = -(j + 0.5) + height / 2.;    // this flips the image at the same time
+            float dir_y = -(j + 0.5) + height / 2.;
             float dir_z = -height / (2. * tan(fov / 2.));
             Vec3f dir = Vec3f(dir_x, dir_y, dir_z);
-            framebuffer[i + j * width] = cast_ray(Vec3f(0, 0, 0), dir,tr);
+
+            Triangle plusproche = triangles.front();
+            Vec3f inter;
+            float dist = DIST_MAX;
+            for (Triangle t : triangles) {
+                Vec3f intersection;
+                if (t.rayIntersect(Vec3f(0, 0, 0), dir, intersection)) {
+                    int disttr = intersection * intersection;
+                    if (disttr < dist) {
+                        plusproche = t;
+                        dist = disttr;
+                        inter = intersection;
+                    }
+
+                }
+
+            }
+            if (dist != 156) {  //Si le rayon intersecte un triangle
+                    framebuffer[i + j * width] = Vec3f(0.4, 0.4, 0.3);
+            }
+            else { //Si le rayon n'intersecte pas de triangle, on affiche la couleur de fond
+                framebuffer[i + j * width] = Vec3f(0.2, 0.7, 0.8);
+            }
+
+            //printf("%i %i\n", i, j);
         }
+        printf("%i \n", j);
     }
 
     std::ofstream ofs; // save the framebuffer to file
@@ -137,15 +164,16 @@ void render() {
     for (size_t i = 0; i < height * width; ++i) {
         Vec3f& c = framebuffer[i];
         float max = std::max(c[0], std::max(c[1], c[2]));
-        if (max > 1) c = c * (1. / max);
-        for (size_t j = 0; j < 3; j++) {
-            ofs << (char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
-        }
+            if (max > 1) c = c * (1. / max);
+            for (size_t j = 0; j < 3; j++) {
+                ofs << (char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
+            }
     }
     ofs.close();
 }
 
-int main() {
-    render();
+//Prend en argument le nom du fichier .obj a afficher
+int main(int argc, char* argv[]) {
+    render(getTriangle(argv[1]));
     return 0;
 }
